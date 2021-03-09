afe02bec274393b0f1a35505c5753ce2132c4387
yuling <yulinglin@protonmail.com>
2021-03-09 11:39:48 +0800

    Added fetch in handlesubmit and edited the post route in server/index.js

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,3 +1,8 @@
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1"
+
+
+//post request
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -5,12 +10,23 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
+    
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    fetch('http://localhost:8081/addData',{
+        method: 'POST',
+        credentials: 'same-origin',
+       mode: 'cors',
+        headers:{
+            'Content-Type':'application/json',
+        }, 
+        body:JSON.stringify({sentence: formText}),
+        
     })
+    
+   
+
+    /*postData('/addData', { sentence: formText })*/
+  
 }
 
 export { handleSubmit }
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,3 +1,4 @@
+const projectData = {}
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -11,9 +12,21 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Cors for cross origin allowance
+const cors = require("cors")
+app.use(cors())
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -23,5 +36,27 @@ app.listen(8081, function () {
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.send(projectData)
 })
+
+//post method route
+
+
+app.post('/addData', function(req, res){
+    let data = req.body.sentence
+    projectData = {data};
+    res.send(projectData)
+})
+
+//Try later
+/*app.post('/addData', async function(req, res) {
+    console.log(req.body)
+    const result = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=myApiKey&lang=auto&of=json&url=${req.body.sentence}`);
+    try {
+        const response = await result.json();
+        res.send(response)
+    } catch (error) {
+        console.log("error", error);
+    }})
+    */
+    
